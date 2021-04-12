bc7e77f1272e4794a3761ef015ae6ced49bdb2cb
Thomas Jensen <44089656+thjendk@users.noreply.github.com>
2021-04-12 21:00:04 +0200

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect, useHistory } from "react-router";
 import { ReduxState } from "store";
+import authReducer from "store/auth";
 
 export default function Login() {
   const { register, handleSubmit } = useForm();
   const user = useSelector((state: ReduxState) => state.auth.user);
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const onSubmit = async (values: { username: string; password: string }) => {
     await axios.post("/api/login", values);
+    const res = await axios.get("/api/user");
+    if (!res.data) return;
+    dispatch(authReducer.actions.setUser(res.data));
     history.push("/");
   };
 
